ca9fc42cea505e17a4804f5f566ad267f4a62994
iashyam <shyam10kwd@gmail.com>
2026-07-22 22:00:46 +0530

    generating text with google genai works

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/joho/godotenv"
+	"google.golang.org/genai"
+)
 
 func main() {
-	fmt.Println("hello world")
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Can't laod .env")
+	}
+
+	// googeApiKey := os.Getenv("GOOGLE_API_KEY")
+	model := "gemini-flash-latest"
+	ctx := context.Background()
+	config := &genai.GenerateContentConfig{Temperature: genai.Ptr[float32](0.5)}
+	client, err := genai.NewClient(ctx, nil)
+	if err != nil {
+		log.Fatal("Can't create clinet")
+	}
+	chat, err := client.Chats.Create(ctx, model, config, nil)
+	if err != nil {
+		log.Fatal("Can't create chat")
+	}
+	result, err := chat.SendMessage(ctx, genai.Part{Text: "Why is the sky blue?, answer in 100 words"})
+	if err != nil {
+		log.Fatal("Can't ask questions")
+	}
+	fmt.Println(result.Text())
+
 }
